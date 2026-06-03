65d2ddca389bcbe22ac0d378df0acef6a74763b5
Mo <mhijazi1@github.com>
2026-06-03 15:05:09 -0400

    Handle PR comments in payload
    Add handling for PR comments in main.go

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package sample
 
+	if prRef.Comments != nil {Add a comment on  lines R268 to R270Add diff commentMarkdown input: hybrid mode selected.WritePreviewHybridBold(command b) command⌘ bBItalic(command i) command⌘ iILink(command k) command⌘ kKCode(command e) command⌘ eEQuote(shift command period) shift⇧ command⌘ period.Unordered list(shift command 8) shift⇧ command⌘ 88Ordered list(shift command 7) shift⇧ command⌘ 77Task list(shift command l) shift⇧ command⌘ lLAttachmentCode blockHeadingStrikethrough(shift command x) shift⇧ command⌘ xXMore Formatting tools items 11Private previewLeave a commentCancelCommentStart a review
+		payload["comments"] = prRef.Comments
+	}
+
 import (
 	"crypto/des"
 	"crypto/md5"
