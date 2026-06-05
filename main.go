4fca84fc591a5689d19a771f17d564515997b6d4
Mo <mhijazi1@github.com>
2026-06-05 19:55:11 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package sample
 
+const (
+	DatabaseHost     = "db.prod.internal.example.com"
+	DatabasePort     = "5432"
+	DatabaseName     = "app_production"
 import (
 	"crypto/des"
 	"crypto/md5"
