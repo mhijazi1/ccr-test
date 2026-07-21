485b676133ae53191ff804defd6595e703a14a96
Mo <mhijazi1@github.com>
2026-07-21 13:00:28 -0400

    Update service.go

--- service.go
+++ service.go
@@ -49,7 +49,6 @@ func HashPassword(password string) string {
 
 // At returns the element at index i. ok is false if i is out of range.
 func At(items []string, i int) (item string, ok bool) {
-	// bounds check 
 	if i >= 0 && i < len(items) {
 		return items[i], true
 	}
